7a79171da25a861dc36dde9136c238525b4439ab
Antonio Galea <antonio.galea@gmail.com>
2018-12-07 09:01:52 +0100

    fix: lazy forced unmount is sometimes needed after quitting chroot

--- scripts/grub.sh
+++ scripts/grub.sh
@@ -5,4 +5,4 @@ grub-install --target=i386-pc --boot-directory=/scratch/mnt/boot --recheck /dev/
 
 for d in dev proc sys; do mount -o bind /$d /scratch/mnt/$d; done
 chroot /scratch/mnt/ /usr/sbin/grub-mkconfig -o /boot/grub/grub.cfg
-for d in dev proc sys; do umount /scratch/mnt/$d; done
+for d in dev proc sys; do umount -lf /scratch/mnt/$d; done
